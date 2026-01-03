cc0b64a18bd0628fef98e2c3248ec11ee50683cc
piyush wanhere <wanherepiyush@gmail.com>
2026-01-04 01:42:56 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='medical_chatbot',
+    version='0.1.0',
+    author='Piyush Wanhere',
+    packages=find_packages(),
+    author_email="wanherepiyush@gmail.com",
+    install_requires=[]
+)
