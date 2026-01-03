e2e81fb11f4ffa1f39a53dc55d2004fa885043f3
piyush wanhere <wanherepiyush@gmail.com>
2026-01-04 01:02:01 +0530

    Added Folder structure

---
+++ template.sh
@@ -0,0 +1,16 @@
+#creating directory
+mkdir -p SRC
+mkdir -p research
+
+#creating files
+touch SRC/__init__.py
+touch SRC/main.py
+touch SRC/helper.py
+touch SRC/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directory and files created successfully!."
